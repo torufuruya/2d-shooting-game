1d3fa93466925581e5bde575d68c573b599d5fc9
Toru <toru.furuya@gree.net>
2016-11-30 19:58:35 +0900

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+
+		Vector2 direction = new Vector2 (x, y).normalized;
+		GetComponent<Rigidbody2D> ().velocity = direction * speed;
+	}
+}
