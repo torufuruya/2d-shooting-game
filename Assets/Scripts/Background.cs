4e12d61ef56f66488ee07eae4e57ccc1ef2b5709
Toru Furuya <t.furuya825@gmail.com>
2016-12-04 15:41:58 +0900

    Background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float y = Mathf.Repeat (Time.time * speed, 1);
+
+		Vector2 offset = new Vector2 (0, y);
+
+		GetComponent<Renderer> ().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	}
+}
